9c14d3c102a8dece60a3a4e6f56d4c8620a403b0
Sarah Sindet <79209462+sindetisarah@users.noreply.github.com>
2023-12-21 10:59:58 +0300

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -357,3 +357,30 @@
     separator: true, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
   });
 })();
+
+ /**
+   * Trusted by Brands
+   */
+
+ $(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
